8164d5efe4c098c72bcc575c3158ed367b859200
julienblain <blain.julien@gmail.com>
2018-11-26 17:21:02 +0100

    security

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils) {
+        /*AuthentifactionOutils permet d'avoir une erreur si bad mdp
+        lastUsername permet de savoir le dernier user tappé
+        */
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
